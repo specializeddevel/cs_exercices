ca9560a746a112c4d97c68fc605c622aef483c47
Raul Burgos Murray <specializeddevel@gmail.com>
2024-08-19 21:12:54 -0300

    Added basic API project

---
+++ cursoAPI/MyFirstAPI/MyFirstAPI/Controllers/PeopleController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+
+
+        [HttpGet("{id}")]
+        public People GetId(int id) => Repository.People.First(x => x.Id == id);
+
+        [HttpGet("search/{search}")]
+        public List<People> GetName(String search) =>
+            Repository.People.Where(p => p.Name.ToUpper().Contains(search.ToUpper())).ToList();
+
+    }
+
+    public class Repository
+    {
+        public static List<People> People = new List<People> { 
+            
+            new People()
+            {
+                Id = 1, Name = "Pedro", Birthdate = new DateTime(1990, 12,3)
+            },
+            new People()
+            {
+                Id = 2, Name = "Luis", Birthdate = new DateTime(1992, 11,3)
+            },
+            new People()
+            {
+                Id = 3, Name = "Ana", Birthdate = new DateTime(1985, 1,8)
+            },
+            new People()
+            {
+                Id = 4, Name = "Hugo", Birthdate = new DateTime(1995, 1,30)
+            }
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+}
